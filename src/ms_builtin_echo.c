3880def27e3d664236ab01c37d41513ceddb811b
wleite <wleite@student.42sp.org.br>
2021-11-08 23:15:03 -0300

    fix: added handling for multiples -n before a string

--- src/ms_builtin_echo.c
+++ src/ms_builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 16:07:26 by wleite            #+#    #+#             */
-/*   Updated: 2021/11/08 21:30:52 by wleite           ###   ########.fr       */
+/*   Updated: 2021/11/08 23:12:12 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,22 @@ void	alt_echo(char **cmd)
 {
 	int	i;
 	int	new_line;
+	int	printed_a_line;
 
 	i = 0;
 	new_line = 1;
+	printed_a_line = 0;
 	if (cmd)
 	{
 		while (cmd[++i])
 		{
-			if (ft_strncmp(cmd[i], "-n", 3) == 0 && i == 1)
+			if (ft_strncmp(cmd[i], "-n", 3) == 0 && !printed_a_line)
 			{
 				new_line = 0;
 				continue ;
 			}
 			ft_putstr_fd(cmd[i], 1);
+			printed_a_line = 1;
 			if (cmd[i + 1])
 				ft_putstr_fd(" ", 1);
 		}
