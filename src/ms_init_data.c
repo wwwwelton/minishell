845d88de670a7d7b4645e5fadb7887ec1e85181e
wleite <wleite@student.42sp.org.br>
2021-11-11 19:17:08 -0300

    fix: duplicate environ to data envp

--- src/ms_init_data.c
+++ src/ms_init_data.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ms_init_data.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jofelipe <jofelipe@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 11:32:06 by jofelipe          #+#    #+#             */
-/*   Updated: 2021/11/11 15:37:02 by jofelipe         ###   ########.fr       */
+/*   Updated: 2021/11/11 19:15:40 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	init_env(t_data *node)
 		node->alt_env[i] = NULL;
 	i = -1;
 	while(__environ[++i])
-		node->alt_env[i] = __environ[i];
+		node->alt_env[i] = ft_strdup(__environ[i]);
 }
 
 void	init_data(t_data **data)
