90e7ec338a11dab382f844bd3500ced62721b509
wleite <wleite@student.42sp.org.br>
2021-11-12 03:57:54 -0300

    test: added envp to pwd built-in function test

--- unit/builtin/test_pwd.c
+++ unit/builtin/test_pwd.c
@@ -6,15 +6,35 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 16:12:50 by wleite            #+#    #+#             */
-/*   Updated: 2021/11/11 18:01:22 by wleite           ###   ########.fr       */
+/*   Updated: 2021/11/12 03:50:10 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(int argc, char **argv)
+static void	init_env2(char	*alt_env[200], char **envp)
 {
+	int	i;
+
+	i = -1;
+	while (++i < 200)
+		alt_env[i] = NULL;
+	i = -1;
+	while(envp[++i])
+		alt_env[i] = ft_strdup(envp[i]);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	int		i;
+	int		exit_code;
+	char	*alt_env[200];
+
 	(void)argc;
-	alt_pwd(argv);
-	return (0);
+	init_env2(alt_env, envp);
+	exit_code = alt_pwd(argv, alt_env);
+	i = -1;
+	while (++i < 200)
+		ftex_null_ptr((void *)&alt_env[i]);
+	return (exit_code);
 }
