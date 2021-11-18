7b4ddd6f342761b823b0e3808f8a6930aab25c65
VLN37 <54167882+VLN37@users.noreply.github.com>
2021-11-18 15:32:36 -0300

    chore: remove magic numbers

--- src/ms_executer_builtin.c
+++ src/ms_executer_builtin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ms_executer_builtin.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
+/*   By: jofelipe <jofelipe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 23:20:40 by wleite            #+#    #+#             */
-/*   Updated: 2021/11/18 03:26:56 by wleite           ###   ########.fr       */
+/*   Updated: 2021/11/18 15:29:56 by jofelipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,8 +32,8 @@ void	execute_builtin(int *fd_tmp, t_data *data, int i)
 	int		fd[2];
 	char	**cmd;
 
-	backup[0] = dup(0);
-	backup[1] = dup(1);
+	backup[0] = dup(STDIN_FILENO);
+	backup[1] = dup(STDOUT_FILENO);
 	cmd = data->cmd[i];
 	pipe(fd);
 	if (data->flags[i]->heredoc)
@@ -43,6 +43,6 @@ void	execute_builtin(int *fd_tmp, t_data *data, int i)
 	call_builtin(data->head, cmd, data->alt_env);
 	fd_tmp[0] = fd[0];
 	close(fd[1]);
-	dup2(backup[0], 0);
-	dup2(backup[1], 1);
+	dup2(backup[0], STDIN_FILENO);
+	dup2(backup[1], STDOUT_FILENO);
 }
