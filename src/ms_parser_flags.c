a9de8a8d14eaa6fc886f5f767e8e79945d1b8f52
VLN37 <54167882+VLN37@users.noreply.github.com>
2021-11-22 18:42:09 -0300

    feat: parsing multiple redirections

--- src/ms_parser_flags.c
+++ src/ms_parser_flags.c
@@ -6,7 +6,7 @@
 /*   By: jofelipe <jofelipe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 16:07:10 by jofelipe          #+#    #+#             */
-/*   Updated: 2021/11/22 17:07:15 by jofelipe         ###   ########.fr       */
+/*   Updated: 2021/11/22 18:38:17 by jofelipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,15 +33,22 @@ void	fill_redir_out(t_flags *flags, char *str, char **cmd, int index)
 	i = 0;
 	while (*str)
 	{
-		if (*str == '<')
+		if (*str == '>')
 		{
-			if (*(str + 1) == '<')
+			if (*(str + 1) == '>')
 			{
+				while (flags->redir_out[i].file_out)
+					i++;
+				flags->redir_out[i].file_out = ft_strdup(cmd[index++ + 1]);
 				flags->redir_out[i++].out_append = 1;
 				str++;
 			}
 			else
+			{
+				while (flags->redir_out[i].file_out)
+					i++;
 				flags->redir_out[i++].file_out = ft_strdup(cmd[index++ + 1]);
+			}
 		}
 		str++;
 	}
@@ -58,11 +65,18 @@ void	fill_redir_in(t_flags *flags, char *str, char **cmd, int index)
 		{
 			if (*(str + 1) == '<')
 			{
+				while (flags->redir_in[i].file_in)
+					i++;
+				flags->redir_in[i].file_in = ft_strdup(cmd[index + 1]);
 				flags->redir_in[i++].heredoc = 1;
 				str++;
 			}
 			else
-				flags->redir_in[i++].file_in = ft_strdup(cmd[index++ + 1]);
+			{
+				while (flags->redir_in[i].file_in)
+					i++;
+				flags->redir_in[i++].file_in = ft_strdup(cmd[index + 1]);
+			}
 		}
 		str++;
 	}
@@ -95,19 +109,19 @@ void	identify_flags(t_flags *flags, t_builtin *builtins, char *cmd)
 	{
 		fill_redir_in(flags, tmp[i], tmp, i);
 		fill_redir_out(flags, tmp[i], tmp, i);
-		// remove from here until line 109 to test
-		if (tmp[i][0] == '<')
-		{
-			if (tmp[i][1] == '<')
-				flags->heredoc = 1;
-			flags->file_in = ft_strdup(tmp[i + 1]);
-		}
-		if (tmp[i][0] == '>')
-		{
-			if (tmp[i][1] == '>')
-				flags->out_append = 1;
-			flags->file_out = ft_strdup(tmp[i + 1]);
-		}
+		// remove from here until line 109 to test how it was previously
+		// if (tmp[i][0] == '<')
+		// {
+		// 	if (tmp[i][1] == '<')
+		// 		flags->heredoc = 1;
+		// 	flags->file_in = ft_strdup(tmp[i + 1]);
+		// }
+		// if (tmp[i][0] == '>')
+		// {
+		// 	if (tmp[i][1] == '>')
+		// 		flags->out_append = 1;
+		// 	flags->file_out = ft_strdup(tmp[i + 1]);
+		// }
 	}
 	free_splited_mat(tmp);
 }
--- src/ms_reinit.c
+++ src/ms_reinit.c
@@ -6,11 +6,23 @@
 /*   By: jofelipe <jofelipe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 19:46:16 by jofelipe          #+#    #+#             */
-/*   Updated: 2021/11/16 20:11:17 by jofelipe         ###   ########.fr       */
+/*   Updated: 2021/11/22 17:26:57 by jofelipe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	free_redirs(t_flags *flags)
+{
+	int	i;
+
+	i = -1;
+	while (flags->redir_in[++i].file_in)
+		free(flags->redir_in[++i].file_in);
+	i = -1;
+	while (flags->redir_out[++i].file_out)
+		free(flags->redir_out[++i].file_out);
+}
 
 void	reinit(t_data *data)
 {
@@ -27,7 +39,10 @@ void	reinit(t_data *data)
 		free(data->pat->holder[i]);
 	i = -1;
 	while (data->flags[++i])
+	{
+		free_redirs(data->flags[i]);
 		free(data->flags[i]);
+	}
 	free(data->flags);
 	data->flags = NULL;
 	i = -1;
