6481ad2f0f095f35a12f4cfdb9af74d1efb02d28
wleite <wleite@student.42sp.org.br>
2021-11-26 02:28:04 -0300

    feat: added minishell to env PATH

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 01:29:28 by jofelipe          #+#    #+#             */
-/*   Updated: 2021/11/26 00:39:28 by wleite           ###   ########.fr       */
+/*   Updated: 2021/11/26 02:25:59 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,13 @@
 
 char	**g_envp;
 
-int	main(void)
+int	main(int argc, char **argv, char **envp)
 {
 	t_data		*data;
 
+	(void)argc;
+	(void)argv;
+	g_envp = envp;
 	init_data(&data);
 	// printfunctions(head);
 	while (1)
--- src/ms_init_data.c
+++ src/ms_init_data.c
@@ -6,11 +6,28 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 11:32:06 by jofelipe          #+#    #+#             */
-/*   Updated: 2021/11/26 00:39:02 by wleite           ###   ########.fr       */
+/*   Updated: 2021/11/26 02:26:55 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	append_minishell_path(void)
+{
+	char	*shell;
+	char	*path;
+	char	*slash;
+
+	shell = get_env_val("_", g_envp);
+	slash = ft_strrchr(shell, '/');
+	ft_memset(slash, '\0', ft_strlen(slash));
+	shell = ftex_str_replace(shell, "/.", "");
+	shell = ftex_strmerge(ft_strdup(":"), shell);
+	path = get_env_val("PATH", g_envp);
+	path = ftex_strmerge(path, shell);
+	set_env_val("PATH", path, g_envp);
+	ftex_null_ptr((void *)&path);
+}
 
 void	init_env(t_data *node)
 {
@@ -21,9 +38,10 @@ void	init_env(t_data *node)
 		node->alt_env[i] = NULL;
 	i = -1;
 	node->alt_env[++i] = ft_strdup("?=0");
-	while (__environ[++i])
-		node->alt_env[i] = ft_strdup(__environ[i]);
+	while (g_envp[++i])
+		node->alt_env[i] = ft_strdup(g_envp[i]);
 	g_envp = node->alt_env;
+	append_minishell_path();
 }
 
 void	init_data(t_data **data)
