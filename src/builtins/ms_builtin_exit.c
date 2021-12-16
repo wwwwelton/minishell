2df16a70b823d26d57870d90dd9f1b1a5993c60d
wleite <wleite@student.42sp.org.br>
2021-12-15 21:01:11 -0300

    fix: fixed exit return values

--- src/builtins/ms_builtin_exit.c
+++ src/builtins/ms_builtin_exit.c
@@ -3,24 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ms_builtin_exit.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jofelipe <jofelipe@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 16:07:26 by wleite            #+#    #+#             */
-/*   Updated: 2021/11/27 16:52:11 by jofelipe         ###   ########.fr       */
+/*   Updated: 2021/12/15 20:46:10 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-static int	too_many_arguments(char **str)
-{
-	if (str && str[1] && str[2])
-	{
-		ft_putendl_fd("exit: too many arguments", 2);
-		return (EXIT_FAILURE);
-	}
-	return (EXIT_SUCCESS);
-}
 
 static int	is_numeric(char *str)
 {
@@ -31,6 +21,16 @@ static int	is_numeric(char *str)
 		if (!ft_isdigit(str[i]))
 			return (0);
 	return (1);
+}
+
+static int	too_many_arguments(char **str)
+{
+	if (str && str[1] && is_numeric(str[1]) && str[2])
+	{
+		ft_putendl_fd("exit: too many arguments", 2);
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
 }
 
 static void	printf_invalid_parameter(char *str)
@@ -44,11 +44,11 @@ int	alt_exit(char **str, t_data *data)
 {
 	(void)data;
 	if (too_many_arguments(str))
-		return (cleanup(data, EXIT_FAILURE));
+		return (EXIT_FAILURE);
 	if (str[1] && !is_numeric(str[1]))
 	{
 		printf_invalid_parameter(str[1]);
-		return (cleanup(data, EXIT_FAILURE));
+		return (cleanup(data, 2));
 	}
 	else if (str[1])
 		return (cleanup(data, ft_atoi(str[1])));
